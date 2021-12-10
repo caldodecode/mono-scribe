6bfd05346f83f7620de68eb6092eef80a8e83d1c
gabriel-bianchessi <gabriel.bianchessi21@gmail.com>
2021-12-10 16:14:55 -0300

    Added Css Injection function

--- addons/gabrielb/addon.js
+++ addons/gabrielb/addon.js
@@ -1,3 +1,15 @@
-export default function() {
+export default function () {
+
+  function injectCSS() {
+    const x = document.createElement("link")
+    x.rel = "stylesheet"
+    let p = import.meta.url
+    p = p.substring(0, p.lastIndexOf("/"))
+    x.href = p + "/style.css"
+    document.head.appendChild(x)
+  }
+
+  injectCSS();
+
   console.log("Hello, World!")
 }
