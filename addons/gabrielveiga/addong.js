c2352dea61599c56826454b099bfebb8ceff98d2
Gabriel Veiga <gabrielveiga54@gmail.com>
2021-12-16 17:44:52 -0200

    mudanças da sexta

--- addons/gabrielveiga/addong.js
+++ addons/gabrielveiga/addong.js
@@ -1,4 +1,23 @@
 export default function () {
-    document.body.style.backgroundColor = "orange",
-    
+    const fileName = import.meta.url
+    const dirName = fileName.substring(0, fileName.lastIndexOf("/"))
+
+    const link = document.createElement("link")
+    link.rel="stylesheet"
+    link.type="text/css" 
+    link.media="screen" 
+    link.href=`${dirName}/addong.css` 
+    document.head.appendChild(link)
+
+    // document.body.style.backgroundColor = "pink"
+    // document.body.style.setProperty("--error-color", "indigo")
+    // document.body.style.setProperty("--text-color", "LightYellow")
+    // document.body.style.setProperty("--sub-color", "black")
+
+
+
+
+    const vogal = ["a", "e", "i", "o", "u"]
+
+
 }
