643662f41b565b235c303723e8a8775b441c2491
elias-cribas <elias.coelho92004@gmail.com>
2021-12-16 16:20:29 -0200

    *TEST* Audio source added

--- addons/elias/addon.js
+++ addons/elias/addon.js
@@ -1,5 +1,8 @@
 export default function() {
     document.onkeydown = function() {
+        var audio = new Audio('audio.mp3');
+        audio.play();
+
         var x = Math.floor(Math.random() * 256);
         var y = Math.floor(Math.random() * 256);
         var z = Math.floor(Math.random() * 256);
