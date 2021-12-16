2f8fae2c598ea82ba38e5174d12c909f0b488653
OtavioLobo <otaviolobo1234@gmail.com>
2021-12-16 17:40:40 -0300

    alterei o css

---
+++ addons/otavio/addon.js
@@ -0,0 +1,14 @@
+export default function myfunction() {
+    document.body.style.setProperty('--bg-color','#101820')
+    document.body.style.setProperty('--main-color','#eedaea')
+    document.body.style.setProperty('--caret-color', '#eedaea')
+    document.body.style.setProperty('--sub-color', '#cf6bdd')
+    document.body.style.setProperty('--error-color', '#ff5253')
+    document.body.style.setProperty('--error-extra-color', '#e3002b;')
+    document.body.style.setProperty('--colorful-error-color', '#ff5253;')
+    document.body.style.setProperty('--colorful-error-extra-color', '#e3002b')
+
+}
+
+myfunction()
+
