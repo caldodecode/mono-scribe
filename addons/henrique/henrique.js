a9eba4f86dbff18cfcfc173d723f620562df0a3b
Alunos <henriquezmello@hotmail.com>
2021-12-16 18:00:35 -0200

    atualização da branch

--- addons/henrique/henrique.js
+++ addons/henrique/henrique.js
@@ -1,3 +1,17 @@
 export default function () {
-    
+    var keyLeft = [65,83,68,81,87,69,90]
+    //asdqwezxc, fghjrtyuvbnm, kliop
+
+    document.addEventListener("update-stats", ev => {
+        
+        const grupo = ["a", "b", "c"]
+        
+        if (grupo.includes(ev.key)) {
+            console.log("aewww")
+            return
+        }
+        
+        console.log("não tem grupo") 
+    })
+
 }
