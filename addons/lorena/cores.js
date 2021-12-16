13b96a1a8445f71e4ace4b48b7b778943f2a0fe7
Lorena Marilia da Silva <lorenamarilia2005@gmail.com>
2021-12-16 18:48:36 -0200

    Criei o cores.js e aterei o css

---
+++ addons/lorena/cores.js
@@ -0,0 +1,9 @@
+export default function colores(){
+    document.body.style.backgroundColor = "#a1c9c9",
+    document.body.style.setProperty('--sub-color', '#34a0a4'),
+    document.body.style.setProperty('--text-color', '#f6f7f1'),
+    document.body.style.setProperty('--error-color', '#d55152'),
+    document.body.style.setProperty('--caret-color', '#e5c99f')
+}
+
+colores()
