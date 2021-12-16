70699882356957240e2395c4153da84667c72faf
RaffaelSchemmer <elias.coelho92004@gmail.com>
2021-12-16 20:14:29 -0300

    Testing with images

--- addons/elias/addon.js
+++ addons/elias/addon.js
@@ -1,16 +1,31 @@
 export default function() {
+    let i = 0
+
+    function change() {
+        const fileName =
+            import.meta.url
+        const dirName = fileName.substring(0, fileName.lastIndexOf("/"));
+        var color = [`${dirName}/imagens/sus.gif`];
+        document.body.style.backgroundImage = color[i];
+        i = (i + 1) % color.length;
+    }
+    setInterval(change, 1000);
+
     document.onkeydown = function() {
-        const fileName = import.meta.url
+        const fileName =
+            import.meta.url
         const dirName = fileName.substring(0, fileName.lastIndexOf("/"));
         var audio = new Audio(`${dirName}/audio.mp3`);
         audio.play();
-
-        var x = Math.floor(Math.random() * 256);
-        var y = Math.floor(Math.random() * 256);
-        var z = Math.floor(Math.random() * 256);
-        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        console.log(bgColor);
-        document.body.style.background = bgColor;
     }
-
 }
+
+// CHANGE COLOR WHEN YOU TYPE
+
+//     // var x = Math.floor(Math.random() * 256);
+//     // var y = Math.floor(Math.random() * 256);
+//     // var z = Math.floor(Math.random() * 256);
+//     // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+//     // console.log(bgColor);
+//     // document.body.style.background = bgColor;
+//
