23ca68a09df916887310674a254102eba3b2f952
Heloísa <helodc.online@gmail.com>
2021-12-18 11:15:57 -0300

    Mudar a cor de fundo ao atingir determinado numero de caracteres

--- addons/heloisa/addon.js
+++ addons/heloisa/addon.js
@@ -4,29 +4,39 @@ export default function() {
 
     document.addEventListener("keydown", ev => {
         qtd++
+        
+        if (ev.key == "Shift"
+            || ev.key == "Control"
+            || ev.key == "Escape"
+            || ev.key == "AltGraph"
+            || ev.key == "ContextMenu"
+            || ev.key == "Tab"
+            || ev.key == "Backspace"){
+            qtd = qtd - 1
+        }
 
         if (qtd == 15) {
-            document.body.style.backgroundColor = "#F6AE9D";
+            document.body.style.backgroundColor = "#BFA39D";
             return
         }
 
         if (qtd == 30) {
-            document.body.style.backgroundColor = "#D494AF";
+            document.body.style.backgroundColor = "#9E7888";
             return
         }
 
         if (qtd == 45) {
-            document.body.style.backgroundColor = "#DCB1EB";
+            document.body.style.backgroundColor = "#B591C2";
             return
         }
 
         if (qtd == 60) {
-            document.body.style.backgroundColor = "#9E94D4";
+            document.body.style.backgroundColor = "#7F78AB";
             return
         }
 
         if (qtd == 75) {
-            document.body.style.backgroundColor = "#ACCBF6";
+            document.body.style.backgroundColor = "#8FA8CC";
             qtd = 0;
             return
         }
