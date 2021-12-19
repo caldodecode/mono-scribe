be36c1150e2674f94974cc860924aea4d75b620a
gabriel-bianchessi <gabriel.bianchessi21@gmail.com>
2021-12-19 18:42:09 -0300

    Finished Doom Fire implementation

--- addons/gabrielb/addon.js
+++ addons/gabrielb/addon.js
@@ -18,14 +18,28 @@ export default async function () {
 let answers = {
   correct: 0,
   wrong: 0,
-  time:0
+  time: 0
 }
 
-window.addEventListener('update-stats', ev => {
-  answers.correct = ev.stats.corrects
-  answers.wrong = ev.stats.errors
-  answers.time = ev.stats.time
+window.addEventListener('update-stats', ({ stats }) => {
+  if (stats.type !== "key") 
+    return;
+  
+  if(!stats.lastIsCorrect) {
+    decreaseFireSource()
+    return
+  }
+
+  increaseFireSource()
+
+  answers.correct = stats.corrects
+  answers.wrong = stats.errors
+  answers.time = stats.time
+  
+  console.log(stats)
 })
+
+setInterval(() => decreaseFireSource(), 500)
 
 
 //Fogo bixo
@@ -34,36 +48,12 @@ let fireCanvas = document.createElement("div")
 fireCanvas.id = "fireCanvas"
 document.body.appendChild(fireCanvas)
 
-let fireCanvasButtons = document.createElement("div")
-fireCanvasButtons.id = "fireCanvasButtons"
-fireCanvasButtons.innerHTML = `<div class="margin">
-<button id="decreaseFireButton">-</button>
-<button id="minFireButton">Min Fire</button>
-<button id="maxFireButton">Max Fire</button>
-<button id="increaseFireButton">+</button>
-</div>
-<div class="margin">
-<button id="toggleDebugModeButton">Toggle Debug Mode</button>
-</div>`
-document.body.appendChild(fireCanvasButtons)
-
-const minFireButton = document.querySelector('#minFireButton')
-minFireButton.onclick = destroyFireSource
-
-const maxFireButton = document.querySelector('#maxFireButton')
-maxFireButton.onclick = createFireSource
-
-const increaseFireButton = document.querySelector('#increaseFireButton')
-increaseFireButton.onclick = increaseFireSource
-
-const decreaseFireButton = document.querySelector('#decreaseFireButton')
-decreaseFireButton.onclick = decreaseFireSource
-
-const toggleDebugModeButton = document.querySelector('#toggleDebugModeButton')
-toggleDebugModeButton.onclick = toggleDebugMode
+let screenHeight = window.innerHeight
+let screenWidth = window.innerWidth
+console.log(screenHeight, screenWidth)
 
 const firePixelsArray = []
-let fireWidth = 40
+let fireWidth = 60
 let fireHeight = 40
 let debug = false
 const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
