bc551e0ae66dff505a3ebed9fbba217250d18520
RaffaelSchemmer <elias.coelho92004@gmail.com>
2021-12-22 14:11:19 -0300

    Added loop music, type sound, and every time you type the background will change to image

--- addons/elias/addon.js
+++ addons/elias/addon.js
@@ -1,24 +1,64 @@
 export default function() {
-    setInterval(
-        function() {
-            var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-            document.body.style.backgroundColor = "#" + randomColor;
-        }, 1000);
+    alert(
+        "WHAT MY ADDON DOES: This addon will change the background to a funny random image every time you type a word"
+    );
 
-    document.onkeydown = function() {
-        const fileName =
-            import.meta.url
-        const dirName = fileName.substring(0, fileName.lastIndexOf("/"))
-        var audio = new Audio(`${dirName}/audio.mp3`);
-        audio.play();
+    alert(
+        "* WARNING *\nThis site can trigger seizures for people with photosensitive epilepsy\n* WARNING *"
+    );
+
+    const image = [
+        "url(addons/elias/imagens/amogus-talking.gif)",
+        "url(addons/elias/imagens/amour-amongus.gif)",
+        "url(addons/elias/imagens/sus.gif)",
+        "url(addons/elias/imagens/therock.gif)",
+        "url(addons/elias/imagens/trollface.gif)",
+    ];
+    const fileName =
+        import.meta.url;
+    const dirName = fileName.substring(0, fileName.lastIndexOf("/"));
+
+    var i = 0;
+
+    function changeImage() {
+        var doc = document.body.style;
+        if (i >= image.length) {
+            i = 0;
+        }
+
+        doc.backgroundImage = image[i];
+        doc.backgroundRepeat = "no-repeat";
+        doc.backgroundSize = "cover";
+        doc.backgroundPosition = "center";
+
+        i++;
+        console.log(i);
     }
+
+    document.addEventListener("update-stats", (ev) => {
+        if (event.stats.type == "key") {
+            const audio = new Audio(`${dirName}/funny_audios/audio.mp3`);
+            audio.play();
+            audio.volume = 0.1;
+            changeImage();
+            return;
+        }
+    });
+
+    const insaneSoundtrack = new Audio(`${dirName}/funny_audios/realDrip.mp3`);
+    if (typeof insaneSoundtrack.loop == "boolean") {
+        insaneSoundtrack.loop = true;
+    } else {
+        insaneSoundtrack.addEventListener(
+            "ended",
+            function() {
+                this.currentTime = 0;
+                this.play();
+            },
+            false
+        );
+    }
+    insaneSoundtrack.play();
+    insaneSoundtrack.volume = 0.3;
+
 }
-
-// CHANGE COLOR WHEN YOU TYPE
-
-//     // var x = Math.floor(Math.random() * 256);
-//     // var y = Math.floor(Math.random() * 256);
-//     // var z = Math.floor(Math.random() * 256);
-//     // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-//     // console.log(bgColor);
-//     // document.body.style.background = bgColor;
